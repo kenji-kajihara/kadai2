63a10779857df2f3f69791372da3f8e6fd8e9a57
kenji_kajihara <kenji_kajihara@b-engineer.jp>
2019-07-02 01:53:15 +0000

    php/laravel 11 kadai

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Route::group(['prefix' => 'admin'], function() {
 Route::get('XXX','AAAController@bbb');
 
 Route::group(['prefix' => 'admin'], function() {
-     Route::get('profile/create', 'Admin\PrfileController@add');
+     Route::get('profile/create', 'Admin\ProfileController@add');
      Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
